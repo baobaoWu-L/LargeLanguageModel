2dcc9a1e4b61e9346960abed5b0a934aee538a01
LoveBreaker <zhangzipeng26@outlook.com>
2025-12-10 13:19:31 +0800

    修改了openai，现在适用于通义千问

--- app/config.py
+++ app/config.py
@@ -5,7 +5,8 @@ import os
 load_dotenv()  # 将操作系统中的环境变量读取一下
 
 class Settings(BaseModel):  # BaseModel后续这个类会转换为JSON/字典方便使用
-    openai_api_key: str = os.getenv("OPENAI_API_KEY", "sk-6ceac0ac789d4a4ead54060a4af23724")
+    qianwen_api_key: str = os.getenv("QIANWEN_API_KEY", "sk-6ceac0ac789d4a4ead54060a4af23724")
+    qianwen_embedding_model_name: str = os.getenv("QIANWEN_EMBEDDING_MODEL_NAME", 'text-embedding-v2')
     model_name: str = os.getenv("MODEL_NAME", "qwen-plus")
     chroma_dir: str = os.getenv("CHROMA_DIR", "./data/chroma")
     chroma_host: str = os.getenv("CHROMA_HOST", "localhost")
@@ -13,5 +14,6 @@ class Settings(BaseModel):  # BaseModel后续这个类会转换为JSON/字典方
     collection_name: str = os.getenv("COLLECTION_NAME", "knowledge_base")
     chunk_size: int = int(os.getenv("CHUNK_SIZE", "800"))
     chunk_overlap: int = int(os.getenv("CHUNK_OVERLAP", "120"))
+
 
 settings = Settings()
